5a4b3a6b65eaa23854d93723cb87a63aa595c696
edenlum <edenlum@gmail.com>
2021-03-30 16:22:49 +0300

    Pool working
    seeing circles

--- RayTracer.py
+++ RayTracer.py
@@ -1,9 +1,13 @@
 import numpy as np
 import sys
 from graphics import *
+from numpy.linalg import norm
+from matplotlib import pyplot as plt
 
 class Scene:
-    def __init__(self,):
+    def __init__(self, width : int, height : int):
+        self.width = width # number of pixels
+        self.height = height # number of pixels
         self.materials = []
         self.spheres = []
         self.boxes = []
@@ -33,14 +37,67 @@ class Scene:
                 if line[0] == "lgt":
                     self.lights.append(Light(*line[1:]))
 
+    def ray_cast(self,):
+        image = np.zeros((self.width, self.height, 3))
+        pixel_size = self.camera.width / self.width
+        height = self.height * pixel_size
+        self.camera.calc_axis()
+        center_screen = self.camera.pos + self.camera.dist * self.camera.Vz
+        print(center_screen)
+        p0 = center_screen - self.camera.Vx * self.camera.width/2 - self.camera.Vy * height/2
+        print(p0)
+        for i in range(self.width):
+            p = p0.copy()
+            for j in range(self.height):
+                ray = Ray(self.camera.pos, p - self.camera.pos)
+                hit_point, mat_idx = self.find_intersection(ray)
+                image[i, j, :] = self.get_color(mat_idx)
+                p += self.camera.Vx * pixel_size
+            p0 += self.camera.Vy * pixel_size
+        return image
+
+
+    def find_intersection(self, ray : Ray):
+        min_t = np.Infinity
+        mat_idx = None
+        for sphere in self.spheres:
+            inter = sphere.intersect2(ray)
+            if inter == None: continue
+            t, idx = inter
+            # print(t)
+            if t < min_t and t>=0:
+                min_t = t
+                mat_idx = idx
+        for plane in self.planes:
+            inter = plane.intersect(ray)
+            t, idx = inter
+            if t < min_t and t>=0:
+                min_t = t
+                mat_idx = idx
+        hit_point = ray.p0 + ray.vec*min_t
+        return hit_point, mat_idx
+
+    def get_color(self, mat_idx):
+        if mat_idx == None:
+            return self.Settings.bg
+        color = self.materials[mat_idx-1].get_color(self.Settings.bg)
+        # color = np.minimum(color, np.ones(3))
+        # color /= np.max(color)
+        return color
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Not enough arguments provided. Please specify an input scene file and an output image file for rendering.")
+        exit()
     scene_name = sys.argv[1]
     image_name = sys.argv[2]
     res = (500, 500)
     if len(sys.argv) > 3:
         res = (int(sys.argv[3]), int(sys.argv[3]))
 
-    scene = Scene()
+    scene = Scene(res[0], res[1])
     scene.parse_scene(scene_name)
+    image = scene.ray_cast()
+    plt.imshow(image)
+    plt.show()
--- graphics.py
+++ graphics.py
@@ -1,18 +1,57 @@
 import numpy as np
+from numpy.linalg import norm
+
+
+class Ray:
+    def __init__(self, pos : np.array, vec : np.array):
+        self.p0 = pos
+        self.vec = vec
+        self.vec /= norm(vec)
 
 
 class Sphere:
     def __init__(self, x : str, y : str, z : str, radius : str, mat_idx : str):
-        self.pos = (float(x),float(y),float(z))
+        self.pos = np.array([float(x),float(y),float(z)])
         self.radius = float(radius)
         self.mat_idx = int(mat_idx)
+
+    def intersect(self, ray : Ray):
+        a = 1
+        d = ray.p0 - self.pos
+        b = 2*np.dot(ray.vec, d)
+        c = np.sum(d**2) - self.radius **2
+        sol = quad(a, b, c)
+        if sol == None:
+            return None
+        t1, t2 =sol
+        return min(t1, t2), self.mat_idx
+
+    def intersect2(self, ray: Ray):
+        l = self.pos - ray.p0
+        t_ca = np.dot(l, ray.vec)
+        if (t_ca < 0):
+            return None
+        d_sq = np.dot(l, l) - t_ca**2
+        if (d_sq > self.radius**2):
+            return None
+        t_hc = np.sqrt(self.radius**2 - d_sq)
+        return (t_ca - t_hc), self.mat_idx # there is also t_ca+t_hc
+
+    def find_normal(self, p : np.array):
+        n = (p-self.pos)/norm(p-self.pos)
+        return n
+
 
 
 class Plane:
     def __init__(self, nx : str, ny : str, nz : str, offset : str, mat_idx : str):
-        self.normal = (float(nx), float(ny), float(nz))
+        self.normal = np.array([float(nx), float(ny), float(nz)])
         self.offset = float(offset)
         self.mat_idx = int(mat_idx)
+
+    def intersect(self, ray : Ray):
+        t = (self.offset - np.dot(ray.p0, self.normal)) / (np.dot(ray.vec, self.normal))
+        return t, self.mat_idx
 
 
 class Box:
@@ -34,11 +73,16 @@ class Box:
 class Material:
     def __init__(self, dr : str, dg : str, db : str, sr : str, sg : str, sb : str, rr : str,
                  rg : str, rb : str, phong : str, trans : str):
-        self.dif = (float(dr), float(dg), float(db))
-        self.spec = (float(sr), float(sg), float(sb))
+        self.dif = np.array([float(dr), float(dg), float(db)])
+        self.spec = np.array([float(sr), float(sg), float(sb)])
+        self.ref = np.array([float(rr), float(rg), float(rb)])
         self.phong = float(phong)
-        self.ref = (float(rr), float(rg), float(rb))
         self.trans = float(trans)
+
+    def get_color(self, bgc : np.array):
+        self.color = bgc * self.trans + (self.dif + self.spec) * (1-self.trans) + self.ref
+        return self.color
+
 
 
 class Light:
@@ -61,15 +105,15 @@ class Light:
 class Camera:
     def __init__(self, x : str, y:str, z:str, lx : str, ly:str, lz:str, ux : str, uy:str,
                  uz:str, dist : str, width : str, fish_eye : str = False, k : str = 0.5):
-        self.pos = (float(x), float(y), float(z))
+        self.pos = np.array([float(x), float(y), float(z)])
         self.x = float(x)
         self.z = float(z)
         self.y = float(y)
-        self.lpos = (float(lx), float(ly), float(lz))
+        self.lpos = np.array([float(lx), float(ly), float(lz)])
         self.lx = float(lx)
         self.ly = float(ly)
         self.lz = float(lz)
-        self.upos = (float(ux), float(uy), float(uz))
+        self.upos = np.array([float(ux), float(uy), float(uz)])
         self.ux = float(ux)
         self.uy = float(uy)
         self.uz = float(uz)
@@ -78,16 +122,31 @@ class Camera:
         self.fish_eye = bool(fish_eye)
         self.k = float(k)
 
+    def calc_axis(self,):
+        self.Vz = self.lpos - self.pos # direction the camera is looking at (viewing direciton)
+        self.Vz /= norm(self.Vz)
+        self.up = self.upos # up direction of the world
+        self.Vx = np.cross(self.up, self.Vz) # left direction of the Camera
+        self.Vx /= norm(self.Vx)
+        self.Vy = np.cross(self.Vz, self.Vx) # up direction of the camera (Vx, Vy, Vz are orthogonal)
+        self.Vy /= norm(self.Vy)
+
 
 class Settings:
     def __init__(self, bgr : str, bgg : str, bgb : str, shadow_num : str, rec_level : str):
-        self.bg = (float(bgr), float(bgg), float(bgb))
+        self.bg = np.array([float(bgr), float(bgg), float(bgb)])
         self.shadow_num = int(shadow_num)
         self.rec_level = int(rec_level)
 
 
+def reflection(normal : np.array, ray : Ray, point : np.array):
+    direction = ray.vec - 2*normal * np.dot(ray.vec, normal)
+    new_ray = Ray(point, direction)
+    return new_ray
+
+
 def quad(a,b,c):
     if(b**2>=4*a*c):
-        return ((-b+np.sqrt(b**2-4*a*c))/2*a,(-b-np.sqrt(b**2-4*a*c))/2*a)
+        return ((-b+np.sqrt(b**2-4*a*c))/(2*a),(-b-np.sqrt(b**2-4*a*c))/(2*a))
     else:
         return None
