d370bf0fdc287a1822079dedab45b5b9b34a9f6c
edenlum <edenlum@gmail.com>
2021-03-30 11:02:05 +0300

    Update graphics.py
    added plane and box

--- graphics.py
+++ graphics.py
@@ -2,6 +2,17 @@ import numpy as np
 
 
 class Sphere:
-    def __init__(self, pos : Tuple, radius : int):
+    def __init__(self, pos : Tuple, radius : float):
         self.pos = pos
         self.radius = radius
+
+class Plane:
+    def __init__(self, normal : Tuple, offset : float):
+        self.normal = normal
+        self.offset = offset
+
+class Box:
+    def __init__(self, pos : Tuple, scale : Tuple, rotation : Tuple):
+        self.pos = pos
+        self.scale = scale
+        self.rotation = rotation
