2366512fad454151860728568798800f0b9bd9cb
tamirco2002 <tamirco2200@gmail.com>
2021-03-30 11:21:04 +0300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -26,5 +26,24 @@ class Material:
         self.ref = ref
         self.trans = trans
 
+class Light:
+    def __init__(self, pos : Tuple, color : float,spec : float,shadow : float,factor : float,radius : float):
+        self.pos = pos
+        self.color = color
+        self.spec = spec
+        self.shadow = shadow
+        self.factor = factor
+        self.radius = radius
 
-
+class Camera:
+    def __init__(self, pos : Tuple, LAP : Tuple,up : Tuple,dist : float,width : float,bg : float,shadow_num : int,
+                 rec_level : int,fish_eye : bool):
+        self.pos = pos
+        self.LAP = LAP
+        self.up = up
+        self.dist = dist
+        self.width = width
+        self.bg = bg
+        self.shadow_num = shadow_num
+        self.rec_level = rec_level
+        self.fish_eye = fish_eye
