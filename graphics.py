fa25b97d430ad3c8c3bd1e90e61fd3c782fcaf74
tamirco2002 <tamirco2200@gmail.com>
2021-03-30 12:32:53 +0300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -9,50 +9,53 @@ class Sphere:
 
 
 class Plane:
-    def __init__(self, nx : float, ny : float, nz : float, offset : float, mat_idx : int):
+    def __init__(self, nx : string, ny : string, nz : string, offset : string, mat_idx : string):
         self.normal = (float(nx), float(ny), float(nz))
         self.offset = float(offset)
         self.mat_idx = int(mat_idx)
 
 
 class Box:
-    def __init__(self, x : float,y:float,z:float, sx : float,sy : float,sz : float, rotx : float,roty : float,rotz : float, mat_idx : int):
-        self.pos = (x,y,z)
-        self.x=x
-        self.y=y
-        self.z=z
-        self.sx = sx
-        self.sy = sy
-        self.sz = sz
-        self.rotx = rotx
-        self.roty = roty
-        self.rotz = rotz
-        self.mat_idx = mat_idx
+    def __init__(self, x : string,y:string,z:string, sx : string,sy : string,sz : string, rotx : string,roty : string,
+                 rotz : string, mat_idx : string):
+        self.pos = (float(x),float(y),float(z))
+        self.x=float(x)
+        self.y=float(y)
+        self.z=float(z)
+        self.sx = float(sx)
+        self.sy = float(sy)
+        self.sz = float(sz)
+        self.rotx = float(rotx)
+        self.roty = float(roty)
+        self.rotz = float(rotz)
+        self.mat_idx = int(mat_idx)
 
 
 class Material:
-    def __init__(self, dr : float, dg : float, db : float, sr : float, sg : float, sb : float, rr : float, rg : float, rb : float, phong : float, trans : float):
-        self.dif = (dr, dg, db)
-        self.spec = (sr, sg, sb)
-        self.phong = phong
-        self.ref = (rr, rg, rb)
-        self.trans = trans
+    def __init__(self, dr : string, dg : string, db : string, sr : string, sg : string, sb : string, rr : string,
+                 rg : string, rb : string, phong : string, trans : string):
+        self.dif = (float(dr), float(dg), float(db))
+        self.spec = (float(sr), float(sg), float(sb))
+        self.phong = float(phong)
+        self.ref = (float(rr), float(rg), float(rb))
+        self.trans = float(trans)
 
 
 class Light:
-    def __init__(self, x : float, y:float, z:float, r : float, g : float, b : float, spec : float, shadow : float,radius : float):
-        self.pos = (x, y, z)
-        self.x = x
-        self.y = y
-        self.z = z
-        self.color = (r, g, b)
-        self.r = r
-        self.g = g
-        self.b = b
-        self.spec = spec
-        self.shadow = shadow
-        self.factor = 1-shadow
-        self.radius = radius
+    def __init__(self, x : string, y:string, z:string, r : string, g : string, b : string, spec : string,
+                 shadow : string,radius : string):
+        self.pos = (float(x), float(y), float(z))
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+        self.color = (float(r), float(g), float(b))
+        self.r = float(r)
+        self.g = float(g)
+        self.b = float(b)
+        self.spec = float(spec)
+        self.shadow = float(shadow)
+        self.factor = 1-float(shadow)
+        self.radius = float(radius)
 
 
 class Camera:
