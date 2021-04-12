22abdeb7b60abb2592b1e7ead44be3fdee8b395b
tamirco2002 <tamirco2200@gmail.com>
2021-04-12 09:11:08 +0300

    intersect_vec box

--- graphics.py
+++ graphics.py
@@ -109,20 +109,18 @@ class Box:
             t, _, _ = plane.intersect_vec(p0, v)
             hit_point = p0 + v*t[:, np.newaxis]
             if(i<=1):
-                condition=hit_point[1]>=(self.y - self.size/2) and hit_point[1]<=(self.y + self.size/2)
-                and hit_point[2]>=(self.z - self.size/2) and hit_point[2]<=(self.z + self.size/2)
-                t=np.where(condition, np.inf, [plane,hit_point])
-                intersecting_planes.append([plane,hit_point, np.norm(p0-hit_point)])
+                condition=hit_point[1]>=(self.y - self.size/2) and hit_point[1]<=(self.y + self.size/2)\
+                          and hit_point[2]>=(self.z - self.size/2) and hit_point[2]<=(self.z + self.size/2)
             elif (i<=3):
-                if (hit_point[2] >= (self.z - self.size / 2) and hit_point[2] <= (self.z + self.size / 2)
-                and hit_point[0] >= (self.x - self.size / 2) and hit_point[0] <= (self.x + self.size / 2)):
-                    intersecting_planes.append([plane,hit_point, np.norm(p0-hit_point)])
+                condition = hit_point[2] >= (self.z - self.size / 2) and hit_point[2] <= (self.z + self.size / 2)\
+                            and hit_point[0] >= (self.x - self.size / 2) and hit_point[0] <= (self.x + self.size / 2)
             else:
-                if (hit_point[1] >= (self.y - self.size / 2) and hit_point[1] <= (self.y + self.size / 2)
-                and hit_point[0] >= (self.x - self.size / 2) and hit_point[0] <= (self.x + self.size / 2)):
-                    intersecting_planes.append([plane,hit_point, np.norm(p0-hit_point)])
-
-        np.argmin(intersecting_planes,axis=2)
+                condition = hit_point[1] >= (self.y - self.size / 2) and hit_point[1] <= (self.y + self.size / 2)\
+                            and hit_point[0] >= (self.x - self.size / 2) and hit_point[0] <= (self.x + self.size / 2)
+            intersecting_planes.append(np.where(condition, np.inf, [np.norm(p0 - hit_point),plane.normal,t]))
+        intersecting_planes=np.array(intersecting_planes)
+        correct_planes=intersecting_planes.sort(intersecting_planes[:,:,0].argsort())[:,0]
+        return correct_planes[,:2], self.mat_idx,correct_planes[,:1]
 
 
 
